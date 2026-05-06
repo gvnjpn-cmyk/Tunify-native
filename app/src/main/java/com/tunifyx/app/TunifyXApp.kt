2a6b5f1d5914bef1c7751e1dd274f5a1bf22ae28
gvnjpn-cmyk <gvnjpn@gmail.com>
2026-05-06 15:29:20 +0700

    Upload: app/src/main/java/com/tunifyx/app/TunifyXApp.kt

---
+++ app/src/main/java/com/tunifyx/app/TunifyXApp.kt
@@ -0,0 +1,10 @@
+package com.tunifyx.app
+
+import android.app.Application
+
+class TunifyXApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // App-level init here if needed
+    }
+}
